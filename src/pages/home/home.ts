7f472367df84ae6988692df1e3028c4ff2fd1b43
SenWangHost <senwang1992010@gmail.com>
2018-05-07 10:39:26 -0400

    start a simple google map mobile app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+
+declare var google: any;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  initMap() {
+    this.map = new google.maps.Map(this.mapElement.nativeElement, {
+      zoom: 7,
+      center: {lat: 41.85, lng: -87.65}
+    });
+  }
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public platform: Platform) {
+    platform.ready().then(() => {
+      this.initMap();
+    });
   }
 
 }
