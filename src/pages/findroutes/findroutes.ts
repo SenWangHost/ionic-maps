a7909e86d8d7f696471a630a653ceecbcae16bd6
SenWangHost <senwang1992010@gmail.com>
2018-05-07 22:21:15 -0400

    finish implementing the autocomplete service for starting point and destination

--- src/pages/findroutes/findroutes.ts
+++ src/pages/findroutes/findroutes.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { ViewController } from 'ionic-angular';
+
+declare var google: any;
 
 @Component({
     selector: 'find-routes',
@@ -7,10 +9,46 @@ import { ViewController } from 'ionic-angular';
   })
 
 export class FindRoutes {
+    @ViewChild('startloc') startElement: ElementRef;
+    @ViewChild('endloc') endElement: ElementRef;
+    startloc:string;
+    endloc:string;
+    autoCompleteStart:any;
+    autoCompleteEnd:any;
     constructor(public viewCtrl: ViewController) {
-
     }
+
+    ngOnInit() {
+        this.autoCompleteStart = new google.maps.places.Autocomplete(this.startElement.nativeElement);
+        this.autoCompleteEnd = new google.maps.places.Autocomplete(this.endElement.nativeElement);
+        this.autoCompleteStart.addListener('place_changed', function() {
+            let place = this.getPlace();
+            if (!place.geometry) {
+                // User entered the name of a Place that was not suggested and
+                // pressed the Enter key, or the Place Details request failed.
+                console.log("No details available for input: '" + place.name + "'");
+                return;
+            }
+            if (place.geometry.viewport) {
+                console.log(place);
+            }
+        })
+        this.autoCompleteEnd.addListener('place_changed', function() {
+            let place = this.getPlace();
+            if (!place.geometry) {
+                // User entered the name of a Place that was not suggested and
+                // pressed the Enter key, or the Place Details request failed.
+                console.log("No details available for input: '" + place.name + "'");
+                return;
+            }
+            if (place.geometry.viewport) {
+                console.log(place);
+            }
+        })
+    }
+
     dismiss() {
         this.viewCtrl.dismiss();
     }
+
 } 
